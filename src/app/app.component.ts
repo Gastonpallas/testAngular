dbd23bcc208d4e00efb357db4f789a258140dc82
Gaston Pallas <pallasgaston@gmail.com>
2023-09-19 16:26:15 +0200

    init Angular Standalone Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone : true,
+  template: `
+    <p>test</p>
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'testAngular';
